db7ed4d80b6ce36392874e1c0b38661cd9b43a8b
Jahno Hacker <jeba760@gmail.com>
2017-10-21 16:19:22 +0100

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="text-center">
+	<p>&copy; 2017 &middot; Laracarte by Jahno</p>
+
+</footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,38 @@
+ @extends('layouts.default')
+
+   @section('contenu')
+
+    <div class="container">
+    	 <h2>What is laracarte</h2>
+    	<p>Laracarte is a clone</p>
+
+    	<div class="row">
+    		<div class="col-md-6">
+    			<div class="alert alert-warning">
+    				hjklm
+    			</div>
+    		</div>
+    	</div>
+
+    	<p>  kkljkj,lknml </p>
+    	<hr>
+
+    	<h2>What is laramap</h2>
+
+    	 <p>laramap is ftyui</p>
+    	 <p>laramap is ftyujhklmi</p>
+    	    <hr>
+    	  <h2>hiuopù</h2>
+    	  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    	  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    	  </p>
+    	 <ul>
+    	 	<li>laravel</li>
+    	 	<li>Amazone</li>
+    	 	<li>Jk</li>
+    	 	<li>lop</li>
+    	 	<li>kl</li>
+    	 </ul>
+
+    </div>
+  @stop
